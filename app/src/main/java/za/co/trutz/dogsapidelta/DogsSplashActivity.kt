b2fbadd2df0a4b6654bd17e1051168ff2435bc83
Tafadzwa Masunda <>
2022-06-21 15:25:19 +0200

    Splash screen logic added

---
+++ app/src/main/java/za/co/trutz/dogsapidelta/DogsSplashActivity.kt
@@ -0,0 +1,25 @@
+package za.co.trutz.dogsapidelta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import za.co.trutz.dogsapidelta.databinding.ActivityDogsSplashBinding
+
+class DogsSplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDogsSplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDogsSplashBinding.inflate(layoutInflater)
+
+        binding.splashScreenContainer.alpha = 0f
+        binding.splashScreenContainer.animate().setDuration(1500).alpha(1f).withEndAction{
+            val openMainScreenIntent = Intent(this, MainActivity::class.java)
+            startActivity(openMainScreenIntent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        setContentView(binding.root)
+    }
+}
